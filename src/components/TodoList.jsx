dcffb6c986efe37062107d5cd3be69ebf4fa6345
Dimitri Ligdamis <ligdamisdimitri@gmail.com>
2023-01-02 21:11:23 +0100

    :art: style: add icon check/edit/delete on list item

--- src/components/TodoList.jsx
+++ src/components/TodoList.jsx
@@ -2,6 +2,9 @@ import { collection, getDocs } from "firebase/firestore";
 import { useEffect, useState } from "react";
 import { db } from "../firebase";
 import { CreateTodo } from "./CreateTodo";
+import { MdEdit } from "react-icons/md";
+import { BsFillCheckCircleFill } from "react-icons/bs";
+import { IoMdTrash } from "react-icons/io";
 
 export const TodoList = () => {
   const [todos, setTodos] = useState([]);
@@ -30,8 +33,16 @@ export const TodoList = () => {
       <CreateTodo reRender={reRender} setReRender={setReRender} />
       <ul className="w-3/4">
         {todos?.map((todo) => (
-          <li className="font-bold rounded my-4 p-3 bg-white" key={todo.id}>
-            {todo.Subject}
+          <li
+            className="flex justify-between font-bold rounded my-4 p-3 bg-white"
+            key={todo.id}
+          >
+            <p className="text-lg">{todo.Subject}</p>
+            <div className="flex items-center gap-5 text-2xl">
+              <BsFillCheckCircleFill className="text-[#5EFE9D]" />
+              <MdEdit className="text-[#10DDF3]" />
+              <IoMdTrash className="text-[#EE5557]" />
+            </div>
           </li>
         ))}
       </ul>
